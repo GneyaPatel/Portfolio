6e6ed0136cbc20e12d046bc3a406b41af90aaac3
GneyaPatel <88178249+GneyaPatel@users.noreply.github.com>
2024-05-04 13:56:40 +0530

    first commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:myemail@email.com">gneyapatel81202@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/gneya-patel-6990b0259">linkedin.com/gneya-patel</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/GneyaPatel">github.com/GneyaPatel</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
